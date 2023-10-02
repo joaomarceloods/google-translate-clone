eb6ed9f4800cb71054df2b3a775fbe279f4cdb2b
João Souza <joaomarcelo9393@gmail.com>
2023-10-02 17:58:07 -0300

    Debounce API calls to save quota

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -1,34 +1,37 @@
 import { useEffect, useState } from "react"
+import debounce from "lodash.debounce"
+
+// Makes a POST request to the Google Translate API
+// https://cloud.google.com/translate/docs/reference/rest/v2/translate
+async function translateText(text, source, target, callback) {
+  const response = await fetch(
+    `https://translation.googleapis.com/language/translate/v2?key=${process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}&q=${text}&source=${source}&target=${target}&format=text`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+
+  ).then(res => res.json())
+
+  const translatedText = response?.data?.translations[0]?.translatedText
+
+  if (translatedText) {
+    callback(translatedText)
+  }
+}
+
+const translateTextDebounced = debounce(translateText, 500, { maxWait: 2000 })
 
 const useTranslation = (text, source, target) => {
   const [translation, setTranslation] = useState("")
 
   useEffect(() => {
-    // Makes a POST request to the Google Translate API
-    // https://cloud.google.com/translate/docs/reference/rest/v2/translate
-    async function translateText() {
-      const response = await fetch(
-        `https://translation.googleapis.com/language/translate/v2?key=${process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}&q=${text}&source=${source}&target=${target}&format=text`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-
-      ).then(res => res.json())
-
-      const translatedText = response?.data?.translations[0]?.translatedText
-
-      if (translatedText) {
-        setTranslation(translatedText)
-      }
-    }
-
     if (source === target) {
       setTranslation(text)
     } else if (text) {
-      translateText()
+      translateTextDebounced(text, source, target, setTranslation)
     } else {
       setTranslation("")
     }
