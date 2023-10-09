9316a106eebe596b682860bd90821d0b54000c37
João Souza <joaomarcelo9393@gmail.com>
2023-10-09 16:41:27 -0300

    Update app manifest and meta tags

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,5 +1,5 @@
 import { GitHub } from "@mui/icons-material";
-import { AppBar, Box, Hidden, Icon, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Link, Toolbar, Typography } from "@mui/material";
 
 const TopAppBar = () => {
   return (
@@ -9,13 +9,8 @@ const TopAppBar = () => {
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Google Translate Clone
           </Typography>
-          <Link href="https://github.com/joaomarceloods" color="inherit" underline="hover" target="_blank">
-            <Hidden smUp>
-              <GitHub />
-            </Hidden>
-            <Hidden smDown>
-              https://github.com/joaomarceloods
-            </Hidden>
+          <Link href="https://github.com/joaomarceloods/google-translate-clone" color="inherit" underline="hover" target="_blank">
+            <GitHub />
           </Link>
         </Toolbar>
       </AppBar>
